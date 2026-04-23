00bb14fa55414bc38ab9005ec0227280a527effc
Princeton Afeez <princetonafeez@gmail.com>
2026-04-23 12:18:13 -0700

    Strategy interface for parsing one date format

--- src/agecalc/parsing.py
+++ src/agecalc/parsing.py
@@ -7,3 +7,11 @@ from typing import Protocol
 from agecalc.exceptions import AmbiguousDateError, InvalidDateError
 
 
+class DateParser(Protocol):
+
+    @property
+    def name(self) -> str:
+        raise NotImplementedError
+
+    def parse(self, raw_value: str) -> date:
+        raise NotImplementedError
