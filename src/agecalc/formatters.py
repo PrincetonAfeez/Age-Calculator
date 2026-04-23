008eaff42ac90bcbaa8a72809f084cede4a15d79
Princeton Afeez <princetonafeez@gmail.com>
2026-04-23 12:40:35 -0700

    Strategy interface for formatting output

--- src/agecalc/formatters.py
+++ src/agecalc/formatters.py
@@ -8,3 +8,21 @@ from agecalc.calculations import Milestone
 from agecalc.domain import Age, Profile
 
 
+class OutputFormatter(Protocol):
+    def format_age(self, age: Age) -> str:
+        raise NotImplementedError
+
+    def format_diff(self, age: Age) -> str:
+        raise NotImplementedError
+
+    def format_milestones(self, milestones: Sequence[Milestone]) -> str:
+        raise NotImplementedError
+
+    def format_profile(self, profile: Profile) -> str:
+        raise NotImplementedError
+
+    def format_profiles(self, profiles: Sequence[Profile]) -> str:
+        raise NotImplementedError
+
+    def format_message(self, message: str) -> str:
+        raise NotImplementedError
