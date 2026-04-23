7ad9f8bdec3122b2946f1c2f3bbf0b9d5cff952c
Princeton Afeez <princetonafeez@gmail.com>
2026-04-23 12:24:44 -0700

    Repository interface for saved profiles

--- src/agecalc/storage.py
+++ src/agecalc/storage.py
@@ -9,3 +9,20 @@ from agecalc.domain import Profile
 from agecalc.exceptions import UnknownProfileError
 
 
+class ProfileRepository(ABC):
+    @abstractmethod
+    def save(self, profile: Profile) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get(self, name: str) -> Profile:
+        raise NotImplementedError
+
+    @abstractmethod
+    def list(self) -> list[Profile]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, name: str) -> None:
+        raise NotImplementedError
+
